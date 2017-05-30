78bff06d8f1bcea25b20cf13d792f87a0b45c02a
snoozemaster <861226sj@gmail.com>
2017-05-30 13:18:01 +0900

    Update main.py
    add class

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
 import telepot
 from pprint import pprint
-
-import datetime 
-import pandas_datareader.data as web  
+import datetime
+import pandas_datareader.data as web
 import time
 
-def store_id_name(_chid, _chname,_msg,_chat,_id,_name,_tx):  #사용불가 함수
-    #말건사람 챗아이디, 이름 저장하기   #call by reference가 안되서 안되는듭..
-    _chid = newresponse[_msg][_chat][_id]
-    _chname = newresponse[_msg][_chat][_name]
+class Person:
+    firstname = 'default'
+    lastname = 'default'
+    id = 'default'
+    msg = 'default'
+    def __init__(self):
+        self.firstname = 'first'
+        self.lastname = 'last'
+        self.id = 0
+        self.msg = 'stock'
 
 def send_start_msg(_chat_id):
-    bot.sendMessage(_chat_id, "웰컴투 수진스탁")
-    bot.sendMessage(_chat_id, "원하는 종목코드를 검색하라(야후파이낸스랑 일치해야함)")
-    bot.sendMessage(_chat_id, "예:삼성전자는 005930.ks 애플은 aapl")
+    bot.sendMessage(_chat_id, "Welcome to Soojin Stock")
+    bot.sendMessage(_chat_id, "Please search the stock by its code (should be same as yahoo finance)")
+    bot.sendMessage(_chat_id, "EX:Samsung Electronics: 005930.ks, Apple: aapl")
 
 #봇 불러오기
 bot = telepot.Bot('351258906:AAGdjeEa817a6AJAyAMok1FfYuO-htbtvxw')
@@ -25,20 +29,23 @@ bot.getMe()
 start = datetime.datetime(2017,1,1)
 store_upid = 1
 get_upid = 0
-errorMsg = "종목 똑바로 넣어람 'ㅗ'"
+errorMsg = 'Try other stocks'
 
 chat_id = None
-chatter_name = '누구냐'
+chatter_name = 'default name'
 
+plist = []
+p1 = Person()
+plist.append(p1)
 
 while(True):
 #    try:
-    time.sleep(3) 
+    time.sleep(1) 
 #봇이 새로운 메세지 있는지 검사    
     wholeresponses =  bot.getUpdates(offset=True)
     newresponse = bot.getUpdates(offset=True)[-1]
     get_upid = newresponse['update_id']    
-    pprint(newresponse)
+#    pprint(newresponse)
 
 #새로운 메세지인지 검사하고, 새로운 메세지일 경우만 아래코드들 시행
     if(get_upid != store_upid):
@@ -51,38 +58,40 @@ while(True):
 #일반챗과 단채쳇은 구조가 달라서 검사하고 진행
     if 'message' in newresponse:    #일반챗임?
         print("일반챗")
-    #말건사람 챗아이디, 이름 저장하기
-        chat_id = newresponse['message']['chat']['id']
-        chatter_name = newresponse['message']['chat']['first_name']
+    #말건사람 챗아이디, 이름, stock 저장하기
+        p1.id = newresponse['message']['chat']['id']
+        p1.firstname = newresponse['message']['chat']['first_name']
+       
 
     #새로운 사용자인지 검사, 인사하기
         start_test = newresponse['message']['text']
         if(start_test=='/start'):
-            send_start_msg(chat_id)
+            send_start_msg(p1.id)
             continue 
     #정보요청한 stock 저장하기    
         requestStock = newresponse['message']['text']
+        plist.append(p1)
     else:                           #단체챗임?
         print("단체챗")
     #말건사람 챗아이디, 이름 저장하기
-        chat_id = newresponse['channel_post']['chat']['id']
-        chatter_name = newresponse['channel_post']['chat']['username']
+        p1.id = newresponse['channel_post']['chat']['id']
+        p1.firstname= newresponse['channel_post']['chat']['username']
 
     #새로운 사용자인지 검사, 인사하기
         start_test = newresponse['channel_post']['text']
         if(start_test=='/start'):
-            send_start_msg(chat_id)
+            send_start_msg(p1.id)
             continue 
     #정보요청한 stock 저장하기    
         requestStock = newresponse['channel_post']['text']
-
+        plist.append(p1)
 
 #사용자가보낸 stock이 실제 있는지 검사, valid한 경우에만 아래코드 시행
     try: 
         result = web.DataReader(requestStock,'yahoo', start,)
     except:
         print("사용자가 잘못 입력함")    
-        bot.sendMessage(chat_id, errorMsg)
+        bot.sendMessage(p1.id, errorMsg)
         pass
         continue
 
@@ -103,27 +112,27 @@ while(True):
     try:
         mktcap = mktcapSoup[0].text
     except:
-        print("시총자료가 존재하지 않음")
-        mktcap = '자료가 엄서영'
+        print("No market cap data")
+        mktcap = 'No market cap data'
         pass
     try:
         per = perSoup[0].text
     except:
-        print("PER자료가 존재하지 않음")
-        per = '자료가 엄서영'
+        print("No PER data")
+        per = 'No PER data'
         pass    
     try:
         div = divsoup[0].text
     except:
-        print("배당자료가 존재하지 않음")
-        div = '자료가 엄서영'
+        print("No Div data")
+        div = 'No Div data'
         pass
 
 #완성된 데이타를 봇이 사용자에게 뿌림
-    bot.sendMessage(chat_id, chatter_name+'님, '
-    + str(requestStock)+'의 현재가는 ' +'\n'+Stock+'\n'+'시총은 $'+mktcap+ ' / '
-    + 'PER은 '+ per + ' / ' + '배당은 '+ div + ' 이당')
-
+    bot.sendMessage(p1.id, p1.firstname+', '
+    + str(requestStock)+':  ' +'\n'+Stock+'\n'+'MarketCap: $'+mktcap+ ' / '
+    + 'PER: '+ per + ' / ' + 'Dividend: '+ div )
+#    print(plist[0].firstname)
 #    except:
 #        print("뭔가 에러남")
 #        pass
